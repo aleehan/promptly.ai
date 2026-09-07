e7133e683e27cf70cb1acb2d302edbf237987912
Murat Alikhan <alihanmurat44@gmail.com>
2026-09-07 10:20:47 +0500

    feat: add autoscroll to the last message

--- src/components/Chat/MessageList.jsx
+++ src/components/Chat/MessageList.jsx
@@ -1,20 +1,28 @@
 import Message from "./Message.jsx";
+import {useEffect, useRef} from "react";
 
 const MessageList = (props) => {
     const {
         messages,
     } = props;
 
+    const bottomRef = useRef(null);
+
+    useEffect(() => {
+        bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, [messages.length])
+
     return (
         <div id="messagesList" className="chat_messages">
             {messages.map((msg) => (
                 <Message
-                key={msg.id}
-                role={msg.role}
-                text={msg.text}
-                timestamp={msg.timestamp}
+                    key={msg.id}
+                    role={msg.role}
+                    text={msg.text}
+                    timestamp={msg.timestamp}
                 />
-                ))}
+            ))}
+            <div ref={bottomRef}/>
         </div>
     )
 }
